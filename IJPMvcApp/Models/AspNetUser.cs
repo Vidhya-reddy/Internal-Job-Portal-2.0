5333bfd5f5ee32a9532bc7032ace1fe651917bb7
Srikanth Itharaju <itharajusrikanth53@gmail.com>
2024-09-03 12:33:54 +0530

    helper

---
+++ IJPMvcApp/Models/AspNetUser.cs
@@ -0,0 +1,9 @@
+﻿namespace IJPMvcApp.Models
+{
+    public class AspNetUser
+    {
+        public string Id { get; set; } = null!;
+
+        public string? UserName { get; set; }
+    }
+}
---
+++ IJPMvcApp/Models/AspNetUserRole.cs
@@ -0,0 +1,8 @@
+﻿namespace IJPMvcApp.Models
+{
+    public class AspNetUserRole
+    {
+        public string UserId { get; set; }
+        public string RoleId { get; set; }
+    }
+}
