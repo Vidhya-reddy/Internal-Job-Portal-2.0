60ef65d73aba50b6a2eac6e75b73cd21d592a480
Srikanth Itharaju <itharajusrikanth53@gmail.com>
2024-09-03 13:16:46 +0530

    commit

--- IJPMvcApp/Models/AspNetUserRole.cs
+++ IJPMvcApp/Models/AspNetUserRole.cs
@@ -9,7 +9,7 @@ public class AspNetUserRole
     {
         public string UserId { get; set; }
         public string RoleId { get; set; }
-        public virtual AspNetUser? User { get; set; }
-        public virtual AspNetRole? Role { get; set; }
+        public string UserName { get; set; }
+        public string RoleName {  get; set; }
     }
-}
+
